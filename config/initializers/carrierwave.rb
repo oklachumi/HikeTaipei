3cf116e7d65f705f3c69a0a1c301f9417802b748
Ray Shih <mnf.shih@gmail.com>
2014-05-24 13:49:44 +0800

    add forgotten files

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,15 @@
+# sample config/initializers/carrierwave.rb
+CarrierWave.configure do |config|
+
+  if Rails.env.development? || Rails.env.test?
+    config.storage = :file
+  else
+    config.storage = :fog
+    config.fog_credentials = {
+      :provider               => 'AWS',
+      :aws_access_key_id      => ENV['S3_KEY'],
+      :aws_secret_access_key  => ENV['S3_SECRET']
+    }
+    config.fog_directory = 'hiketaipei'
+  end
+end
