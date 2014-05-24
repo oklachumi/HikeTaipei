6d5246e438f73838a7589791154fe6d3eeb62a31
Ray Shih <mnf.shih@gmail.com>
2014-05-24 12:06:16 +0800

    initital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HikeTaipei::Application.config.secret_key_base = '20902304b7136025107399ac26418e99288438ff3715078c47b1d17d35509f0930b418f3abcec10b4166b9444d23d73671ba43267b4387533772e4e823d264ca'
